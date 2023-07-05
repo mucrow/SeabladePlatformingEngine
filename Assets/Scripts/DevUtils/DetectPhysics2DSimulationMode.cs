35ce8aa9bf179b2f031b9c7cccf64f63c56d0224
mucrow <120820315+mucrow@users.noreply.github.com>
2023-07-05 18:12:20 -0400

    Initial commit - SLF E02 fully implemented

---
+++ Assets/Scripts/DevUtils/DetectPhysics2DSimulationMode.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Seablade.DevUtils {
+  public class DetectPhysics2DSimulationMode: MonoBehaviour {
+    [SerializeField] SimulationMode2D _requiredSimulationMode;
+
+    void Start() {
+      if (Physics2D.simulationMode != _requiredSimulationMode) {
+        Debug.LogError((
+          "This object requires the Physics2D simulation mode to be set to \"" +
+          _requiredSimulationMode.ToString() + "\" in the project settings. Destroying self."
+        ));
+        Destroy(gameObject);
+      }
+    }
+  }
+}
---
+++ Assets/Scripts/SebLagueFaithful/SLFPlayer.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace Seablade.SebLagueFaithful {
+  [RequireComponent(typeof(SLFController2D))]
+  public class SLFPlayer: MonoBehaviour {
+    [SerializeField] float _gravity = -20f;
+    [SerializeField] float _moveSpeed = 6f;
+
+    SLFController2D _controller;
+    Vector3 _velocity;
+
+    void Start() {
+      _controller = GetComponent<SLFController2D>();
+    }
+
+    void Update() {
+      Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+      _velocity.x = input.x * _moveSpeed;
+      _velocity.y += _gravity * Time.deltaTime;
+      // TODO this (and perhaps other parts of Update()) should be in FixedUpdate()
+      _controller.Move(_velocity * Time.deltaTime);
+    }
+  }
+}
