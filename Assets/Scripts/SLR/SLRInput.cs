1d2bc243f7fcfec7a466a92ecfbfee671dd3891d
mucrow <120820315+mucrow@users.noreply.github.com>
2023-07-06 13:50:15 -0400

    Catch up SLR with SLF

---
+++ Assets/Scripts/SLR/SLRInput.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+
+namespace Seablade.SLR {
+  public class SLRInput {
+    public Key Jump = new Key(KeyCode.Space);
+    public Key MoveLeft = new Key(KeyCode.A);
+    public Key MoveRight = new Key(KeyCode.D);
+    public Key MoveDown = new Key(KeyCode.S);
+    public Key MoveUp = new Key(KeyCode.W);
+
+    public void Poll() {
+      Jump.Poll();
+      MoveLeft.Poll();
+      MoveRight.Poll();
+      MoveDown.Poll();
+      MoveUp.Poll();
+    }
+
+    public void FinishFrame() {
+      Jump.FinishFrame();
+      MoveLeft.FinishFrame();
+      MoveRight.FinishFrame();
+      MoveDown.FinishFrame();
+      MoveUp.FinishFrame();
+    }
+
+    public float GetAxisRawHorizontal() {
+      return GetAxisRaw(MoveLeft, MoveRight);
+    }
+
+    public float GetAxisRawVertical() {
+      return GetAxisRaw(MoveDown, MoveUp);
+    }
+
+    float GetAxisRaw(Key negative, Key positive) {
+      float ret = 0f;
+      if (negative.State) {
+        ret -= 1f;
+      }
+      if (positive.State) {
+        ret += 1f;
+      }
+      return ret;
+    }
+
+    public struct Key {
+      KeyCode _keyCode;
+
+      public bool State;
+      public bool Down;
+      public bool Up;
+
+      public Key(KeyCode keyCode) {
+        _keyCode = keyCode;
+        State = false;
+        Down = false;
+        Up = false;
+      }
+
+      public void Poll() {
+        State = Input.GetKey(_keyCode);
+        if (Input.GetKeyDown(_keyCode)) {
+          Down = true;
+        }
+        if (Input.GetKeyUp(_keyCode)) {
+          Up = true;
+        }
+      }
+
+      public void FinishFrame() {
+        Down = false;
+        Up = false;
+      }
+    }
+  }
+}
--- Assets/Scripts/SLR/SLRPlayer.cs
+++ Assets/Scripts/SLR/SLRPlayer.cs
@@ -1,18 +1,29 @@
-﻿using Seablade.SLF;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Seablade.SLR {
   [RequireComponent(typeof(SLRController2D))]
   public class SLRPlayer: MonoBehaviour {
-    [SerializeField] float _gravity = -20f;
+    // See TRANSLATION NOTE 00000
+    SLRInput _slrInput = new SLRInput();
+
+    [SerializeField] float _jumpHeight = 4f;
+    [SerializeField] float _timeToJumpApex = 0.4f;
     [SerializeField] float _moveSpeed = 6f;
 
+    float _gravity;
+    float _jumpVelocity;
     Vector3 _velocity;
 
     SLRController2D _controller;
 
     void Start() {
       _controller = GetComponent<SLRController2D>();
+
+      // TODO pull this out into a named function. calculating gravity and jump velocity from jump
+      // height and time to jump apex
+      _gravity = (-2f * _jumpHeight) / Mathf.Pow(_timeToJumpApex, 2);
+      _jumpVelocity = Mathf.Abs(_gravity) * _timeToJumpApex;
+      Debug.Log("Gravity: " + _gravity + " Jump Velocity: " + _jumpVelocity);
     }
 
     void FixedUpdate() {
@@ -20,12 +31,31 @@ namespace Seablade.SLR {
         _velocity.y = 0;
       }
 
-      Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+      // See TRANSLATION NOTE 00000
+      Vector2 input = new Vector2(_slrInput.GetAxisRawHorizontal(), _slrInput.GetAxisRawVertical());
+
+      if (_slrInput.Jump.Down && _controller.Collisions.Below) {
+        _velocity.y = _jumpVelocity;
+      }
 
       _velocity.x = input.x * _moveSpeed;
       _velocity.y += _gravity * Time.fixedDeltaTime;
       // TODO pull Time.fixedDeltaTime into a variable (below is the second usage)
       _controller.Move(_velocity * Time.fixedDeltaTime);
+
+      // See TRANSLATION NOTE 00000
+      _slrInput.FinishFrame();
+    }
+
+    //---------------------------------------------------------------------------------------------
+    // TRANSLATION NOTE 00001 (SLF -> SLR): every method below this comment only exists in SLR.
+    //---------------------------------------------------------------------------------------------
+
+    // TRANSLATION NOTE 00000 (SLF -> SLR): because SLF runs its logic in Update as opposed to
+    // FixedUpdate, it can safely poll input directly within its platforming logic. it is necessary
+    // to implement a level of input buffering for SLR (even if it is just a buffer of 1 frame).
+    void Update() {
+      _slrInput.Poll();
     }
   }
 }
